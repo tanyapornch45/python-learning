c6dbaf5697906a151db174ce25b8b6fa0d03c31b
tanyapornch45 <tanyapornch45@gmail.com>
2026-07-07 13:16:34 +0700

    Create Lesson01_Arithmetic and Variables.py

---
+++ Lesson01_Arithmetic and Variables.py
@@ -0,0 +1,9 @@
+# Create variables
+num_years = 2
+days_per_year = 365 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+# Calculate number of hours in 2 years
+total_hours = hours_per_day * days_per_year * num_years
+print (total_hours)
